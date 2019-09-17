8d81cd23ed9bcc86a105b089a13b382d1db6f508
Vinicius Cruvinel Rego <vcruvinelr@gmail.com>
2019-09-17 17:03:39 -0300

    static files settings

--- bid/settings.py
+++ bid/settings.py
@@ -113,6 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ] # pode ter varias pastas de arquivos estaticos.
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home'
 
